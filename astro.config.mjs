da07641e4bb00314cb24e86b73e84fc6e575cf71
Pitfield Digital <hello@pitfielddigital.co.uk>
2026-07-01 18:42:35 +0100

    qb-building-solutions

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,6 @@ import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: 'https://ilkestonaircadets.com',
+  site: 'https://qbbuildingsolutions.com',
   integrations: [sitemap()]
 });
